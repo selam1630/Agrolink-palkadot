13be9902f1fc9633b30c54e5bd2af5bb9d3f6f2a
selam1630 <fikruselam29@gmail.com>
2025-11-02 11:22:32 +0300

    admin profile updated

--- apps/frontend/src/pages/NewPostingPage.tsx
+++ apps/frontend/src/pages/NewPostingPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
 const NewPostingPage: React.FC = () => {
   const navigate = useNavigate();
 
